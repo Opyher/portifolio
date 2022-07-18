5dd6a57e05131a7b4a443f2ab01755391830d5c7
Opyher <102032384+Opyher@users.noreply.github.com>
2022-07-18 11:37:34 -0300

    calculadorav1

---
+++ calculadora/operacao.js
@@ -0,0 +1,11 @@
+let resultado = 0
+
+
+class Operacoes{
+    constructor(num, opr){
+        this.num = num
+        this.opr = opr
+    }
+
+ 
+}
--- calculadora/scripts.js
+++ calculadora/scripts.js
@@ -1,1 +1,53 @@
-document.getElementById('txt-area').innerText = 'oi'
+let button = document.querySelectorAll("button[name='num']")
+
+let num
+let opr = document.querySelectorAll("button[name='opr']")
+let resVisor = document.querySelector("span#res")
+
+
+verificaOpr(opr)
+
+
+button.forEach((numero) => {
+    numero.addEventListener("click", () => {
+        if(resVisor.innerHTML == '0'){
+            resVisor.innerHTML = ''
+        }
+        resVisor.innerHTML += numero.textContent 
+        let num = parseInt(numero.value)
+        
+    })
+})
+
+
+
+function verificaOpr(opr){
+    opr.forEach((element) => {
+        
+        if(element.id == "opr-porc"){
+            element.classList.add("opr-alt")
+        } else {
+            element.classList.add("opr")
+        }
+
+        element.addEventListener("click", () => {
+           
+            if(element.value == '='){
+                resVisor.innerHTML = eval(resVisor.textContent)
+
+            } else {
+                resVisor.innerHTML += element.value
+                
+               
+            }
+        })
+
+    })
+
+}
+
+function limpar(){
+    resVisor.innerHTML = '0'
+}
+
+
